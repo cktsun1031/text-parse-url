f04a84794d64593362cd0ebd5812834742f713f4
RealKoolisw <65409152+RealKoolisw@users.noreply.github.com>
2022-01-07 16:04:27 +0800

    Update index.d.ts

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,11 +1,7 @@
 declare module 'text-parse-url' {
   interface DefaultOption {
-    withPrefixOnly: boolean
-    noEqual: boolean
+    withPrefixOnly?: boolean
+    noEqual?: boolean
   }
   export default function (opt: DefaultOption): any
-  export function contentParseURL(
-    content: string,
-    options: DefaultOption
-  ): Array<string>
 }
